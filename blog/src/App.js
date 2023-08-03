98fbb43cfc0f4d85924964d6838cc3b7a0b7246d
kimkmin06 <kimkmin06@gmail.com>
2023-08-03 23:39:06 +0900

    state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = 'React 강좌';
+  let [title, b] = useState(['HTML','CSS','JS']);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Coding Apple</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ title }</h4>
+        <p>유튜브</p>
+      </div>
+      <div className='list'>
+        <h4>{ title }</h4>
+        <p>유튜브</p>
+      </div>
+      <div className='list'>
+        <h4>{ title }</h4>
+        <p>니코쌤</p>
+      </div>
     </div>
   );
 }
