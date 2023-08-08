792c0105d9532740ed7079d6349cf670896a907e
kimkmin06 <kimkmin06@gmail.com>
2023-08-08 14:41:27 +0900

    component

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,11 +5,11 @@ import { useState } from 'react';
 function App() {
 
   let post = 'React 강좌';
-  let [title, changeTitle] = useState(['HTML','CSS','JS']);
+  let [title, changeTitle] = useState(['HTML', 'CSS', 'JS']);
   let [good, changeGood] = useState(0);
 
   function clickHeart() {
-    changeGood(good+=1);
+    changeGood(good += 1);
   };
 
   return (
@@ -19,7 +19,7 @@ function App() {
       </div>
 
       <button onClick={() => {
-        
+        //가나다순 정렬 코드
       }}>sort</button>
 
       <button onClick={() => {
@@ -29,17 +29,42 @@ function App() {
       }}>edit</button>
 
       <div className="list">
-        <h4>{ title[0] } <span onClick={clickHeart}>♡</span> { good } </h4>
+        <h4>{title[0]} <span onClick={clickHeart}>♡</span> {good} </h4>
         <p>유튜브</p>
       </div>
       <div className='list'>
-        <h4>{ title[1] }</h4>
+        <h4>{title[1]}</h4>
         <p>유튜브</p>
       </div>
       <div className='list'>
-        <h4>{ title[2] }</h4>
+        <h4>{title[2]}</h4>
         <p>니코쌤</p>
       </div>
+
+
+
+      <Modal></Modal>
+
+    </div>
+  );
+}
+
+// let Modal = () => {
+//   return (
+//     <div className='modal'>
+//       <h4>제목</h4>
+//       <p>날짜</p>
+//       <p>상세내용</p>
+//     </div>
+//   );
+// };
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
