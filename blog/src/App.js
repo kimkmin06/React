ff8c3b7f6b7f10986ac528ea4de22bb6c542cd2f
kimkmin06 <kimkmin06@gmail.com>
2023-08-08 18:48:04 +0900

    props

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,11 +6,7 @@ function App() {
 
   let [title, setTitle] = useState(['HTML', 'CSS', 'JS']);
   let [good, setGood] = useState([0, 0, 0]);
-  // let [modal, setModal] = useState(false);
-
-  // function clickHeart() {
-  //   setGood(good += 1);
-  // };
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -18,30 +14,11 @@ function App() {
         <h4>Coding Apple</h4>
       </div>
 
-      {/* <button onClick={() => {
-        let copyTitle = [...title];
-        copyTitle[0] = 'REACT';
-        setTitle(copyTitle);
-      }}>edit</button> */}
-
-      {/* <div className="list">
-        <h4>{title[0]} <span onClick={clickHeart}>♡</span> {good} </h4>
-        <p>유튜브</p>
-      </div>
-      <div className='list'>
-        <h4>{title[1]}</h4>
-        <p>유튜브</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={() => { setModal(!modal) }}>{title[2]}</h4>
-        <p>니코쌤</p>
-      </div> */}
-
       {
         title.map((a, i) => {
           return (
             <div className='list'>
-              <h4>{ title[i] }
+              <h4 onClick={() => { setModal(!modal) }}>{ title[i] }
                 <span onClick={() => {
                   let copyGood = [...good];
                   copyGood[i] += 1;
@@ -54,20 +31,25 @@ function App() {
         })
       }
 
-      {/* {
-        modal ? <Modal></Modal> : null
-      } */}
+      {
+        modal ? <Modal setTitle={setTitle} title={title} /> : null
+      }
 
     </div>
   );
 }
 
-let Modal = () => {
+let Modal = (props) => {
   return (
     <div className='modal'>
-      <h4>제목</h4>
+      <h4>{ props.title[0] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={() => {
+        let copyTitle = [...props.title];
+        copyTitle[0] = "REACT";
+        props.setTitle(copyTitle);
+      }}>edit</button>
     </div>
   );
 };
