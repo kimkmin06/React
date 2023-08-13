b80cf0d082ad00369774f9d425007dfffdc092ad
kimkmin06 <kimkmin06@gmail.com>
2023-08-13 23:07:14 +0900

    router

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
-import data from './data.js'
+import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
@@ -17,33 +18,33 @@ function App() {
 
   return (
     <div className="App">
+
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cats</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      {/* <div className='main-bg' style={{ backgroundImage : 'url(' + banner + ')' }}></div> */}
-      <div className='main-bg'></div>
+      {/* <Link to={"/"}>Home</Link>
+      <Link to={"/detail"}>Cart</Link> */}
 
-      <div className='container'>
-        <Card treats={ treats[0] } i={ treat1 }></Card>
-        <Card treats={ treats[1] } i={ treat2 }></Card>
-        <Card treats={ treats[2] } i={ treat3 }></Card>
-        {/* {
-          treats.map((a, i) => {
-            return (
-              <Card treats={ treats[i] } i={ i }></Card>
-            );
-          })
-        } */}
-      </div>
-
-      <div></div>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className='main-bg'></div>
+            <div className='container'>
+              <Card treats={treats[0]} i={treat1}></Card>
+              <Card treats={treats[1]} i={treat2}></Card>
+              <Card treats={treats[2]} i={treat3}></Card>
+            </div>
+          </>
+        } />
+        <Route path='/detail' element={<div>상세페이지</div>} />
+      </Routes>
 
     </div>
   );
@@ -52,10 +53,10 @@ function App() {
 let Card = (props) => {
   return (
     <div className='col-md-4'>
-      <img src={ props.i } width={"80%"} />
+      <img src={props.i} width={"80%"} />
       {/* <img src={ '이미지 경로' + (props.i+1) + '.jpg' } width={"80%"} /> */}
-      <h4>{ props.treats.title }</h4>
-      <p>{ props.treats.price }</p>
+      <h4>{props.treats.title}</h4>
+      <p>{props.treats.price}</p>
     </div>
   );
 };
