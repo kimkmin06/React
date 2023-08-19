0552490c2795468a0b0204fb6c5f739473005efb
kimkmin06 <kimkmin06@gmail.com>
2023-08-19 22:05:25 +0900

    useParams

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
+
 let Detail = (props) => {
+
+  let {id} = useParams();
+
   return (
     <div className="container">
       <div className="row">
@@ -6,9 +11,9 @@ let Detail = (props) => {
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{ props.treats[0].title }</h4>
-          <p>{ props.treats[0].content }</p>
-          <p>{ props.treats[0].price }원</p>
+          <h4 className="pt-5">{ props.treats[id].title }</h4>
+          <p>{ props.treats[id].content }</p>
+          <p>{ props.treats[id].price }원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
